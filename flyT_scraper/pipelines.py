78d48cb02552c565f6979da73ed52948e43e75b3
asnganje <abd.nganje@gmail.com>
2026-09-09 00:35:27 +0300

    Add Google Sheets pipeline

--- flyT_scraper/pipelines.py
+++ flyT_scraper/pipelines.py
@@ -9,6 +9,11 @@ from itemadapter import ItemAdapter
 from openpyxl import Workbook
 from openpyxl import Workbook
 from openpyxl.styles import Font
+import gspread
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import  os
 
 
 class FlytScraperPipeline:
@@ -50,6 +55,74 @@ class ExcelPipeline:
         return item
     def close_spider(self, spider):
         self.workbook.save("ftigers.xlsx")
+
+class GoogleSheetsPipeline:
+    SCOPES = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    CLIENT_SECRET_FILE = "client_secret.json"
+    TOKEN_FILE = "token.json"
+
+    def open_spider(self, spider):
+
+        credentials = None
+        if os.path.exists(self.TOKEN_FILE):
+            credentials = Credentials.from_authorized_user_file(self.TOKEN_FILE, self.SCOPES, )
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(self.CLIENT_SECRET_FILE, self.SCOPES, )
+                credentials = flow.run_local_server(port=0)
+            with open(self.TOKEN_FILE, "w") as token:
+                token.write(credentials.to_json())
+        self.client = gspread.authorize(credentials)
+        self.spreadsheet = self.client.create("Flying Tiger Products")
+        self.sheet = self.spreadsheet.sheet1
+
+        self.headers = [
+            "Serial No",
+            "Name",
+            "Price",
+            "Product Code",
+            "Image URL",
+            "Product URL"
+        ]
+        self.sheet.append_row(self.headers)
+
+        self.sheet.format(
+            "A1:F1",
+            {
+                "textFormat":{
+                    "bold": True
+                }
+            }
+        )
+        self.serial_number = 1
+        self.spreadsheet.share(
+            None,
+            perm_type="anyone",
+            role="reader"
+        )
+
+    def process_item(self, item, spider):
+        row = [
+            self.serial_number,
+            item['name'],
+            item['price'],
+            item['product_code'],
+            item['image_url'],
+            item['product_url']
+        ]
+
+        self.sheet.append_row(row)
+        self.serial_number += 1
+        return  item
+
+
+
 
 
 
--- flyT_scraper/settings.py
+++ flyT_scraper/settings.py
@@ -59,7 +59,8 @@ DOWNLOAD_DELAY = 1
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "flyT_scraper.pipelines.ExcelPipeline": 300,
+   # "flyT_scraper.pipelines.ExcelPipeline": 300,
+    "flyT_scraper.pipelines.GoogleSheetsPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
