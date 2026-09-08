2dc1635a384d49998306a017270dd7d5b50bbca6
asnganje <abd.nganje@gmail.com>
2026-09-08 18:15:16 +0300

    Add pipeline logic for saving data into excel file

--- flyT_scraper/pipelines.py
+++ flyT_scraper/pipelines.py
@@ -6,8 +6,51 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from openpyxl import Workbook
+from openpyxl import Workbook
+from openpyxl.styles import Font
 
 
 class FlytScraperPipeline:
     def process_item(self, item):
         return item
+class ExcelPipeline:
+
+    def open_spider(self, spider):
+        self.workbook = Workbook()
+        self.sheet = self.workbook.active
+        self.sheet.title = "FTigerProducts"
+
+        headers = [
+            "Serial No",
+            "Name",
+            "price",
+            "Product code",
+            "Image URL",
+            "Product URL"
+        ]
+
+        self.sheet.append(headers)
+
+        for cell in self.sheet[1]:
+            cell.font = Font(bold=True)
+        self.serial_number = 1
+
+    def process_item(self, item, spider):
+        self.sheet.append([
+            self.serial_number,
+            item['name'],
+            item['price'],
+            item['product_code'],
+            item['image_url'],
+            item['product_url']
+        ])
+
+        self.serial_number += 1
+        return item
+    def close_spider(self, spider):
+        self.workbook.save("ftigers.xlsx")
+
+
+
+
--- flyT_scraper/settings.py
+++ flyT_scraper/settings.py
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "flyT_scraper.pipelines.FlytScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "flyT_scraper.pipelines.ExcelPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
